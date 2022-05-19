6a44ce9f1f5ad494f24b899155e6866322000b60
showmittro <showmittrobiswas2000@gmail.com>
2022-05-19 22:11:02 +0600

    delivary button and stock button add and count quntity

---
+++ src/Pages/ManageInventories/ManageInventories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ManageInventories = () => {
+    return (
+        <div>
+            <h1>This is Manage Inventory</h1>
+            
+        </div>
+    );
+};
+
+export default ManageInventories;
--- src/Pages/ServiceDetails/ServiceDetails.js
+++ src/Pages/ServiceDetails/ServiceDetails.js
@@ -2,53 +2,119 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const ServiceDetails = () => {
-
-    const {serviceId} = useParams();
-    const [service, setService] = useState({});
    
 
-    useEffect( () =>{
-        const url = `http://localhost:5000/inventory/${serviceId}`;
-        console.log(url);
-        fetch(url)
-        .then(res=> res.json())
-        .then(data => setService(data));
 
-    }, [])
+    const { serviceId,} = useParams();
+    const [service, setService] = useState({});
+
+
+    useEffect(() => {
+        const url = `http://localhost:5000/inventory/${serviceId}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setService(data));
+
+    }, [serviceId,service])
+
+  
+    const handlStockUpdate = (e) =>{
+        e.preventDefault();
+        let deliver = e.target.quantity.value;
+        const parseDeliver = parseInt(deliver);
+
+        let quantityParse =service.quantity;
+        let quantity = quantityParse + parseDeliver;
+        
+
+        const updateQuntity = {quantity}
+        console.log(updateQuntity)
+
+
+        
+        const url = `http://localhost:5000/inventory/${serviceId}`;
+        fetch(url, {
+            method: "PUT",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify(updateQuntity),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                
+            });
+        
+    }
+
+    
+
+   
+
+    const handleDeliver =  () => {
+        let deliver = 1;
+        let quantityParse =service.quantity;
+        let quantity = quantityParse - deliver;
+        
+
+        const updateQuntity = {quantity}
+        console.log(updateQuntity)
+
+
+        
+        const url = `http://localhost:5000/inventory/${serviceId}`;
+        fetch(url, {
+            method: "PUT",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify(updateQuntity),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                
+            });
+    };
+
+
 
     // <h1>Inventory Detalis</h1>
     //         <h2>You are about to book: {service.name}</h2>
     return (
-        <div>
+        <div className='mt-5'>
             <div class="col d-flex justify-content-center ">
-            <div class="card h-100" style={{ width:'500px',  border:'1px solid yellow'}}>
-      <img src={service.img} class="card-img-top mx-auto pt-4" style={{height:'200px',width:'200px'}} alt="" />
-      <div class="card-body">
-        <h6 class="card-title">Modal: {service.name}</h6>
-        <h6 class="card-title">Supplier: {service.Supplier}</h6>
-        <h6 class="card-title">Price:${service.price}</h6>
-        <h6 class="card-title">Quty:{service.quantity}</h6>
-        <p class="card-text">{service.description}</p>
-        
-        <button type="submit" class="btn btn-primary">Shipped</button>
-        <form className='mt-2'>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label"></label>
-        <input type="number" className='form-label' />
-  </div>
-  <button type="submit" class="btn btn-primary">Restock</button>
-</form>
+                <div class="card h-100" style={{ width: '500px', border: '1px solid yellow' }}>
+                    <img src={service.img} class="card-img-top mx-auto pt-4" style={{ height: '200px', width: '200px' }} alt="" />
+                    <div class="card-body">
+                        <h6 class="card-title">Modal: {service.name}</h6>
+                        <h6 class="card-title">Supplier: {service.Supplier}</h6>
+                        <h6 class="card-title">Price:${service.price}</h6>
+                        <h6 class="card-title">Quty:{service.quantity}</h6>
+                        <p class="card-text">{service.description}</p>
 
-      
-        
-      </div>
-    </div>
-    
-  </div>
-            
+                        <button type="submit" onClick={handleDeliver} class="btn btn-primary">Shipped</button>
 
-            
-            
+                        <form  className='mt-2' onSubmit={handlStockUpdate}>
+                        <div class="mb-3 mt-2">
+                            <label for="exampleInputPassword1" class="form-label"></label>
+                            <input type="number" name='quantity'
+
+                                className='form-label'
+                               
+                        />
+                        </div>
+                        <button type="submit" class="btn btn-primary"
+                          >Restock</button>
+                        </form>
+
+
+
+                    </div>
+                </div>
+
+            </div>
+
+
+            <button  style={{background:'yellow',color:'black'}}  type="button" className="btn  fw-bolder btn-sm mt-5">Manage Inventories</button>
+
         </div>
     );
 };
